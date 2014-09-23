78a48df0b547bf56aad20ccf6e7036f38ed247a0
Gediminas Suklevicius <gediminas.suklevicius@devbridge.com>
2014-09-23 15:37:34 +0300

    Add Sales

---
+++ SalesRegister/SalesRegister/Sales.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SalesRegister
+{
+    public class Sales :  SalesBase, ISales
+    {
+        public string GetItemPrice(string barcode)
+        {
+            string result;
+            
+            if (string.IsNullOrEmpty(barcode))
+            {
+                result = EMPTY_BARCODE;
+            } 
+            else if (Prices.ContainsKey(barcode))
+            {
+                result = Prices[barcode].ToString("C");
+            }
+            else
+            {
+                result = string.Format(PRICE_NOT_FOUND, barcode);
+            }
+
+            return result;
+        }
+    }
+}
