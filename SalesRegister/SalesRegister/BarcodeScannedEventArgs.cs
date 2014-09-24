4e1d92a06d6911ff0fe8b17598cae2c51b70980b
Gediminas Suklevicius <gediminas.suklevicius@devbridge.com>
2014-09-24 16:35:23 +0300

    Some refactoring.

---
+++ SalesRegister/SalesRegister/BarcodeScannedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesRegister
+{
+    public class BarcodeScannedEventArgs : EventArgs
+    {
+        public string Barcode { get; set; }
+    }
+}
---
+++ SalesRegister/SalesRegister/IScanner.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesRegister
+{
+    public interface IScanner
+    {
+        event EventHandler BarcodeScanned;
+    }
+}
